8742f6d4c12478b5439b277f4b4b68a9b42107c8
root <root@rp-VPCEA21EN.(none)>
2013-07-04 23:06:52 +0530

    Hello World from index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+
+var fs = require('fs');
+var read = new Buffer(16);
+read = fs.readFileSync('index.html');
+
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(read.toString());
 });
 
 var port = process.env.PORT || 5000;
